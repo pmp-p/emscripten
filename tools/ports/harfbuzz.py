2db3c74e7685596865b74eda8c9549b9a97f5d07
Paul m. p. P <mail.peny@free.fr>
2020-10-11 09:11:36 +0200

    fix ucdn.c.o not pic compiled

--- tools/ports/harfbuzz.py
+++ tools/ports/harfbuzz.py
@@ -48,7 +48,8 @@ def get(ports, settings, shared):
 
     if settings.RELOCATABLE:
       extra_cxx_flags.append('-fPIC')
-
+      configure_args += ['-DCMAKE_C_FLAGS="-fPIC"']
+      
     if settings.USE_PTHREADS:
       extra_cxx_flags.append('-pthread')
 
